f3a4b1152527f134de1ccfa1a0715a488fd69346
Rufina Litfullina <litllefoot777@gmail.com>
2025-10-09 12:40:35 +0300

    7_delete files from git hub

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     /*     vueDevTools(), */
   ],
+  base: "/sea_battle/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
